674f24fde3d726391196b303c460d0e11aeeb21c
naina-mandhyan <50021931+naina-mandhyan@users.noreply.github.com>
2019-05-13 19:55:00 +0530

    Add files via upload

---
+++ Java-Lab1-13May/com/cg/eis/exception/EmployeeSalary.java
@@ -0,0 +1,23 @@
+package com.cg.eis.exception;
+import java.util.Scanner;
+
+class EmployeeException extends Exception{
+	EmployeeException(String msg){
+		super(msg);
+	}
+}
+
+public class EmployeeSalary{
+
+	public static void main(String[] args) throws EmployeeException {
+			
+		System.out.println("Enter your salary");
+		Scanner scan = new Scanner(System.in);
+		int sal = scan.nextInt();
+		if(sal < 3000)
+			throw new EmployeeException("Sorry, your salary is not up to the mark to have these benefits..");
+		else
+			System.out.println("Getting benefits suggestions...");
+	}
+	
+}
