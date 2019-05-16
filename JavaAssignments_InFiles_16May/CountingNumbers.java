d8ebb2c0b3d4f9096009a41c8cd1b548fe8e65ff
naina-mandhyan <50021931+naina-mandhyan@users.noreply.github.com>
2019-05-16 21:36:07 +0530

    Add files via upload

---
+++ JavaAssignments_InFiles_16May/CountingNumbers.java
@@ -0,0 +1,62 @@
+import java.io.*;
+
+class CountingNumbers{
+	public static void main(String[] args) {
+		try{
+			
+			BufferedReader br =new BufferedReader(new FileReader("otherFile.txt"));
+
+			BufferedReader br1 =new BufferedReader(new FileReader("otherFile.txt"));
+
+			System.out.println("File contains");
+
+			CountingNumbers cn = new CountingNumbers();
+
+			cn.countLinesAndWords(new BufferedReader(new FileReader("otherFile.txt")));
+
+			cn.countCharacters(br);
+
+			br.close();
+		}
+
+		catch(IOException e){
+			System.out.println("File not found");
+		}
+	}
+
+	void countLinesAndWords(BufferedReader br){
+		String newStr = "";
+		String[] str = {};
+		int lineNo = 0,  wordNo = 0;
+
+		try{
+			while((newStr = br.readLine()) != null){
+				lineNo++;
+				str = newStr.split(" ");
+				wordNo += str.length;
+			}
+			
+		}
+
+		catch(IOException e){
+			System.out.println("No lines found");
+		}
+
+		System.out.println(lineNo + " lines\n"+ wordNo + " words");
+	}
+
+	void countCharacters(BufferedReader br){
+		int str = 0;
+		int charNo = 0;
+		try{
+			while((str = br.read()) != -1){
+					charNo++;
+			}
+		
+		}
+		catch(IOException e){
+			System.out.println("File is empty");
+		}
+		System.out.println(charNo + " characters" );
+	}
+}
